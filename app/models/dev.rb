2aa9cf880058dec9d98ac4de56be2f892c6bc0f9
Nicholas Njeru <nicholasnjeru917@gmail.com>
2022-11-06 23:50:26 +0300

    completed mock code challenge

--- app/models/dev.rb
+++ app/models/dev.rb
@@ -1,3 +1,14 @@
 class Dev < ActiveRecord::Base
+has_many :freebies
+has_many :companies, through: :freebies
 
+def received_one?(item_name)
+result = self.freebies.find_by(item_name: item_name)
+result ? true : false
+end
+def give_away(dev, freebie)
+   if self.freebies.include?(freebie) 
+    freebie.update(dev_id: dev.id)
+   end
+end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -11,6 +11,12 @@ Dev.create(name: "Mr. Meseeks")
 Dev.create(name: "Gazorpazop")
 
 puts "Creating freebies..."
+Freebie.create(item_name: "item1", value: 100)
+Freebie.create(item_name: "item2", value: 200)
+Freebie.create(item_name: "item3", value: 300)
+Freebie.create(item_name: "item4", value: 400)
+Freebie.create(item_name: "item5", value: 500)
+
 
 # ***************************************************************
 # * TODO: create freebies! Remember, a freebie belongs to a dev *
